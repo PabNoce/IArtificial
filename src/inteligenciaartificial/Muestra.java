ddbc108294701c17cfcdb99113631646305c593d
pnocedalopez <pnocedalopez@victoria15.danielcastelao.org>
2017-11-09 09:00:50 +0100

    IA sec

--- src/inteligenciaartificial/Muestra.java
+++ src/inteligenciaartificial/Muestra.java
@@ -10,5 +10,14 @@ package inteligenciaartificial;
  * @author pnocedalopez
  */
 public class Muestra {
-    
+
+    String nombre;
+
+    public Muestra() {
+
+    }
+
+    public void nombre(String nombre) {
+        System.out.println("Hola " + nombre);
+    }
 }
--- src/inteligenciaartificial/Pedir.java
+++ src/inteligenciaartificial/Pedir.java
@@ -5,10 +5,20 @@
  */
 package inteligenciaartificial;
 
+import java.util.Scanner;
+
 /**
  *
  * @author pnocedalopez
  */
 public class Pedir {
-    
+
+    String nombre;
+    Scanner sc = new Scanner(System.in);
+
+    public String nombre() {
+        System.out.println("Introduce nombre");
+        return sc.nextLine();
+    }
+
 }
