279545656c51356c51727f1453a16cf16923c083
pnocedalopez <pnocedalopez@victoria15.danielcastelao.org>
2017-11-09 09:22:56 +0100

    IA ultimate

--- src/inteligenciaartificial/InteligenciaArtificial.java
+++ src/inteligenciaartificial/InteligenciaArtificial.java
@@ -1,26 +1,13 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package inteligenciaartificial;
 
-/**
- *
- * @author pnocedalopez
- */
 public class InteligenciaArtificial {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         String nombre;
         Pedir pedir = new Pedir();
         Muestra muestra = new Muestra();
-        nombre=pedir.nombre();
+        nombre = pedir.nombre();
         muestra.nombre(nombre);
-        
 
     }
 
